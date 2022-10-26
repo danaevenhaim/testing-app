bc6b1a4f310d4cd53c86c2c8cfbc73c4b73efd55
danaevenhaim <dana@swimm.io>
2022-10-26 20:34:45 +0300

    test

--- c.py
+++ c.py
@@ -1,2 +1,14 @@
 def lol():
     return 'lalalala'
+
+def lol2():
+    return 'lalalala'
+
+def lol3():
+    return 'lalalala'
+
+def lol4():
+    return 'lalalala'
+
+def lol5():
+    return 'lalalala'
